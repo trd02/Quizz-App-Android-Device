84cdf4b8937736f42f782ce4a73c58563124245b
Trieu Dan <crazysub2002@gmail.com>
2022-11-16 16:21:46 +0700

    fix menu scroll

--- Final_Assignment/app/src/main/java/com/example/quizzapp/MainActivity.java
+++ Final_Assignment/app/src/main/java/com/example/quizzapp/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.quizzapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String selectedTopicName = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
