ff9c57628b3d450e0a0bd379ae1ed6c776e40318
Trieu Dan <crazysub2002@gmail.com>
2022-11-17 18:24:12 +0700

    get topic name from menu to activity

--- Final_Assignment/app/src/main/java/com/example/quizzapp/MainFragment.java
+++ Final_Assignment/app/src/main/java/com/example/quizzapp/MainFragment.java
@@ -1,10 +1,12 @@
 package com.example.quizzapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.strictmode.TargetFragmentUsageViolation;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
@@ -12,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +23,7 @@ import android.widget.Button;
 
 public class MainFragment extends Fragment {
 
+    private String selectedTopicName = "";
     public MainFragment() {
         // Required empty public constructor
     }
@@ -37,10 +41,109 @@ public class MainFragment extends Fragment {
 
         NavController navController = Navigation.findNavController(view);
 
+        //Change selected button scrollview
+        Button dialy = view.findViewById(R.id.diaLy);
+        Button hoahoc = view.findViewById(R.id.hoaHoc);
+        Button vutru = view.findViewById(R.id.vuTru);
+        Button dongvat = view.findViewById(R.id.dongVat);
+        Button thienvan = view.findViewById(R.id.thienVan);
+        Button sinhhoc = view.findViewById(R.id.sinhHoc);
+
+        dialy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedTopicName = "Địa Lý";
+
+                dialy.setBackgroundResource(R.color.teal_200);
+                hoahoc.setBackgroundResource(R.color.purple_700);
+                vutru.setBackgroundResource(R.color.purple_700);
+                dongvat.setBackgroundResource(R.color.purple_700);
+                thienvan.setBackgroundResource(R.color.purple_700);
+                sinhhoc.setBackgroundResource(R.color.purple_700);
+            }
+        });
+
+        hoahoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedTopicName = "Hoá Học";
+
+                hoahoc.setBackgroundResource(R.color.teal_200);
+                dialy.setBackgroundResource(R.color.purple_700);
+                vutru.setBackgroundResource(R.color.purple_700);
+                dongvat.setBackgroundResource(R.color.purple_700);
+                thienvan.setBackgroundResource(R.color.purple_700);
+                sinhhoc.setBackgroundResource(R.color.purple_700);
+            }
+        });
+
+        vutru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedTopicName = "Vũ Trụ";
+
+                vutru.setBackgroundResource(R.color.teal_200);
+                dialy.setBackgroundResource(R.color.purple_700);
+                hoahoc.setBackgroundResource(R.color.purple_700);
+                dongvat.setBackgroundResource(R.color.purple_700);
+                thienvan.setBackgroundResource(R.color.purple_700);
+                sinhhoc.setBackgroundResource(R.color.purple_700);
+            }
+        });
+
+        dongvat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedTopicName = "Động Vật";
+
+                dongvat.setBackgroundResource(R.color.teal_200);
+                dialy.setBackgroundResource(R.color.purple_700);
+                vutru.setBackgroundResource(R.color.purple_700);
+                hoahoc.setBackgroundResource(R.color.purple_700);
+                thienvan.setBackgroundResource(R.color.purple_700);
+                sinhhoc.setBackgroundResource(R.color.purple_700);
+            }
+        });
+
+        thienvan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedTopicName = "Thiên Văn";
+
+                thienvan.setBackgroundResource(R.color.teal_200);
+                dialy.setBackgroundResource(R.color.purple_700);
+                vutru.setBackgroundResource(R.color.purple_700);
+                dongvat.setBackgroundResource(R.color.purple_700);
+                hoahoc.setBackgroundResource(R.color.purple_700);
+                sinhhoc.setBackgroundResource(R.color.purple_700);
+            }
+        });
+
+        sinhhoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedTopicName = "Sinh Học";
+
+                sinhhoc.setBackgroundResource(R.color.teal_200);
+                dialy.setBackgroundResource(R.color.purple_700);
+                vutru.setBackgroundResource(R.color.purple_700);
+                dongvat.setBackgroundResource(R.color.purple_700);
+                hoahoc.setBackgroundResource(R.color.purple_700);
+                thienvan.setBackgroundResource(R.color.purple_700);
+            }
+        });
+
+        //Move to fragment2
         Button button = view.findViewById(R.id.startQuizzBtn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //Đóng gói dữ liệu từ fragment hiện tại
+                Bundle bundle = new Bundle();
+                bundle.putString("selectedTopicName", selectedTopicName);
+                getParentFragmentManager().setFragmentResult("dataFromMenu", bundle);
+                //Khởi chạy fragment tiếp theo
                 navController.navigate(R.id.action_mainFragment_to_quizzActivityFragment);
             }
         });
