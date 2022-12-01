195b04cefd7e4ebb9696cf682267f5cbc18c71eb
DuCLeK65t <llduc.312@gmail.com>
2022-12-02 00:47:06 +0700

    Add Home screen

---
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.blue)));
+    }
+}
